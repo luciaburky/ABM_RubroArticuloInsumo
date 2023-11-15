ddf60e82fbcf28ae72e12a637587af300c289dff
luciaburky <luciaburky1@gmail.com>
2023-11-15 12:05:00 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import * as React from 'react';
 import Spinner from 'react-bootstrap/Spinner';
 import Footer from './components/Footer/Footer';
+import Home from './pages/admin/home/Home';
 
 const Header = React.lazy(() => import('./components/Header/Header'));
 const Admin = React.lazy(() => import('./pages/admin/Admin'));
@@ -11,6 +12,7 @@ function App() {
     <React.Suspense fallback={<Spinner animation="border" />}>
       <BrowserRouter>
         <Header />
+        <Home />
         <Admin />
         <Footer />
       </BrowserRouter>
